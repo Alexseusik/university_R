1983340c8cd6029b5492efde38f4b2948d678d00
Alexseusik <alexgnibeda03@gmail.com>
2023-12-07 00:14:15 +0200

    updating 07.12 0:14

--- lesson1/generator.R
+++ lesson1/generator.R
@@ -1,78 +1,27 @@
-seed_parkmiller <- 1
-seed_mygenerator <- 2^10
-
-park_miller_generator <- function() {
-  a <- 16807
-  m <- 2^31 - 1
+parkmiller_generator <- function() {
   
-  seed_parkmiller <<- (a * seed_parkmiller) %% m
-  return(seed_parkmiller)
+  a <- 16807  
+  c <- 0
+  m <- 2^31-1
+  seed <- 123456789
+  
+  seed <<- (a * seed + c) %% m
+  
+  return(seed)
 }
 
 my_generator <- function() {
-  m <- 2^31
-  a <- 65521
+
+  a <- 65521  
   c <- 3000
-  
-  seed_mygenerator <<- (a * seed_mygenerator + c) %% m
-  return(seed_mygenerator)
-}
-
-n <- 500
-m <- 2^31
-
-generated <- numeric(n)
-
-for (i in 1:n) {
-  generated[i] <- my_generator()
-}
-
-X <- generated/m
-
-plot(1:n, X, cex=0.3)
-
-sorted_X <- sort(X)
-
-plot(sorted_X,(1:n)/n, type='s', xlim=c(0,1), ylim=c(0,1))
-abline(a=0,b=1,col="red")
-
-x1<-X[1:(n-2)]
-x2<-X[2:(n-1)]
-x3<-X[3:n]
-library(rgl)
-
-plot3d(x1,x2,x3)
-plot(x1,x3,cex=0.3)
-
-
-# Ваш лінійний конгруентний генератор
-my_generator <- function() {
   m <- 2^31
-  a <- 65521
-  c <- 3000
+  seed <- 2^10
+
+  seed <- (a * seed + c) %% m
   
-  seed_mygenerator <<- (a * seed_mygenerator + c) %% m
-  return(seed_mygenerator / m)
+  return(seed)
 }
 
-# Ініціалізація seed
-seed_mygenerator <- 1
-
-# Генерація даних
-my_generator_data <- replicate(500, my_generator())
-
-# Генерація даних для розподілу Коші
-cauchy_data <- tan(pi * (my_generator_data - 0.5))
-
-# Сортування даних
-sorted_my_generator_data <- sort(my_generator_data)
-sorted_cauchy_data <- sort(cauchy_data)
-
-# Створення графіків
-plot(sorted_my_generator_data, (1:n)/n, type="l", col="red",
-     main="Cumulative Distribution Functions", xlab="Values", ylab="Cumulative Frequency",
-     xlim=range(c(sorted_my_generator_data, sorted_cauchy_data)),
-     ylim=c(0, 1))
-lines(sorted_cauchy_data, (1:n)/n, col="blue")
-legend("bottomright", legend=c("Linear Congruential Generator", "Cauchy Distribution"),
-       col=c("red", "blue"), lty=1)
+for (i in 1:10) {
+  print(my_generator())
+}
--- lesson2/lesson2.R
+++ lesson2/lesson2.R
@@ -1,20 +1,20 @@
-data <- read.table("lesson2/distr8.txt", header = TRUE)
+data <- read.table("lesson2/distr9.txt", header = TRUE)
 
 X <- sort(data$X)
 
 hist(X, breaks = 50, freq = FALSE, main = "Гістограма з Щільністю Розподілу", xlab = "Значення", ylab = "Щільність")
 lines(density(X), col='red')
 
-plot(pnorm(X, mean = 5, sd = 0.1), (1:length(X))/length(X), asp=1, ylab="", xlab="")
+plot(pnorm(X), (1:length(X))/length(X), asp=1, ylab="", xlab="")
 abline(0,1,col=2)
 
-plot(plnorm(X, meanlog = 2, sdlog = 0.1), (1:length(X))/length(X), asp=1, ylab="", xlab="")
+plot(plnorm(X), (1:length(X))/length(X), asp=1, ylab="", xlab="")
 abline(0,1,col=2)
 
-plot(pexp(X, rate=5), (1:length(X))/length(X), asp=1, ylab="", xlab="")
+plot(pexp(X), (1:length(X))/length(X), asp=1, ylab="", xlab="")
 abline(0,1,col=2)
 
-plot(pchisq(X, df = 5), (1:length(X))/length(X), asp=1, ylab="", xlab="")
+plot(pchisq(X, df = 7), (1:length(X))/length(X), asp=1, ylab="", xlab="")
 abline(0,1,col=2)
 
 qqnorm(X, main = "QQ-діаграма для Нормального Розподілу")
@@ -26,25 +26,21 @@ abline(a = 0, b = 1, col = "red")
 qqplot(qexp(ppoints(X)), X, main = "QQ-діаграма для Експоненційного Розподілу", xlab = "Теоретичні квантилі", ylab = "Зразкові квантилі")
 abline(a = 0, b = 1, col = "red")
 
-qqplot(qchisq(ppoints(X), df = 5), X, main = "QQ-діаграма для χ² Розподілу", xlab = "Теоретичні квантилі", ylab = "Зразкові квантилі")
+qqplot(qchisq(ppoints(X), df = 7), X, main = "QQ-діаграма для χ² Розподілу", xlab = "Теоретичні квантилі", ylab = "Зразкові квантилі")
 abline(a = 0, b = 1, col = "red")
 
 QQplot <- function(x, df, K=1000, alpha=0.05) {
   n <- length(x)
   chiSquareQ <- qchisq(ppoints(n), df = df)
   sx <- sort(x)
-  
-  # Генерація даних для довірчих інтервалів
   W <- matrix(rchisq(K*n, df = df), nrow = n, ncol = K)
   W <- apply(W, 2, sort)
   tops <- apply(W, 1, quantile, probs = 1 - alpha/2)
   bots <- apply(W, 1, quantile, probs = alpha/2)
-  
-  # Побудова діаграми
   plot(c(chiSquareQ, chiSquareQ, chiSquareQ), c(tops, bots, sx), type = "n", xlab = "Теоретичні квантилі", ylab = "Емпіричні квантилі")
-  points(chiSquareQ, sx, col = 2) # дані
-  segments(chiSquareQ, bots, chiSquareQ, tops, col = 4) # довірчі інтервали
-  abline(0, 1, col = 1) # допоміжна лінія
+  points(chiSquareQ, sx, col = 2)
+  segments(chiSquareQ, bots, chiSquareQ, tops, col = 4)
+  abline(0, 1, col = 1)
 }
 
-QQplot(X, 5)
+QQplot(X, 7)
