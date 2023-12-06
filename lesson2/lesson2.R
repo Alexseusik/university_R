cd060d4462593ee634c9fdd88fb622773b0c4334
Alexseusik <alexgnibeda03@gmail.com>
2023-12-06 14:22:44 +0000

    update 06.12

--- lesson2/lesson2.R
+++ lesson2/lesson2.R
@@ -5,6 +5,10 @@ X <- sort(data$X)
 hist(X, breaks = 50, freq = FALSE, main = "Гістограма з Щільністю Розподілу", xlab = "Значення", ylab = "Щільність")
 lines(density(X), col='red')
 
+x <- seq(min(X), max(X), by = 0.1)
+y <- dchisq(x, df)
+lines(x, y, col = "blue")
+
 plot(pnorm(X, mean = 5, sd = 0.1), (1:length(X))/length(X), asp=1, ylab="", xlab="")
 abline(0,1,col=2)
 
@@ -20,31 +24,27 @@ abline(0,1,col=2)
 qqnorm(X, main = "QQ-діаграма для Нормального Розподілу")
 qqline(X, col = "red")
 
-qqplot(qlnorm(ppoints(X)), X, main = "QQ-діаграма для Логнормального Розподілу", xlab = "Теоретичні квантилі", ylab = "Зразкові квантилі")
+qqplot(qlnorm(ppoints(X)), X, main = "QQ-діаграма для Логнормального Розподілу", xlab = "Теоретичні квантилі", ylab = "Емпіричні квантилі")
 abline(a = 0, b = 1, col = "red")
 
-qqplot(qexp(ppoints(X)), X, main = "QQ-діаграма для Експоненційного Розподілу", xlab = "Теоретичні квантилі", ylab = "Зразкові квантилі")
+qqplot(qexp(ppoints(X)), X, main = "QQ-діаграма для Експоненційного Розподілу", xlab = "Теоретичні квантилі", ylab = "Емпіричні квантилі")
 abline(a = 0, b = 1, col = "red")
 
-qqplot(qchisq(ppoints(X), df = 5), X, main = "QQ-діаграма для χ² Розподілу", xlab = "Теоретичні квантилі", ylab = "Зразкові квантилі")
+qqplot(qchisq(ppoints(X), df = 5), X, main = "QQ-діаграма для χ² Розподілу", xlab = "Теоретичні квантилі", ylab = "Емпіричні квантилі")
 abline(a = 0, b = 1, col = "red")
 
 QQplot <- function(x, df, K=1000, alpha=0.05) {
   n <- length(x)
   chiSquareQ <- qchisq(ppoints(n), df = df)
   sx <- sort(x)
-  
-  # Генерація даних для довірчих інтервалів
   W <- matrix(rchisq(K*n, df = df), nrow = n, ncol = K)
   W <- apply(W, 2, sort)
   tops <- apply(W, 1, quantile, probs = 1 - alpha/2)
   bots <- apply(W, 1, quantile, probs = alpha/2)
-  
-  # Побудова діаграми
   plot(c(chiSquareQ, chiSquareQ, chiSquareQ), c(tops, bots, sx), type = "n", xlab = "Теоретичні квантилі", ylab = "Емпіричні квантилі")
-  points(chiSquareQ, sx, col = 2) # дані
-  segments(chiSquareQ, bots, chiSquareQ, tops, col = 4) # довірчі інтервали
-  abline(0, 1, col = 1) # допоміжна лінія
+  points(chiSquareQ, sx, col = 2)
+  segments(chiSquareQ, bots, chiSquareQ, tops, col = 4)
+  abline(0, 1, col = 1)
 }
 
 QQplot(X, 5)
